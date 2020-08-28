ab5c78b3be74a715fb01a32d3f0ab1789b88ab2b
Chris Reparasi <chrisreparasi20@gmail.com>
2020-08-29 00:58:02 +0700

    Commimt 4

--- config/app.php
+++ config/app.php
@@ -163,7 +163,7 @@ return [
         Illuminate\View\ViewServiceProvider::class,
         Yajra\DataTables\DataTablesServiceProvider::class,
         Barryvdh\DomPDF\ServiceProvider::class,
-        RealRashid\SweetAlert\SweetAlertServiceProvider::class,
+        Uxweb\SweetAlert\SweetAlertServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -231,7 +231,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'DataTables' => Yajra\DataTables\Facades\DataTables::class,
         'PDF' => Barryvdh\DomPDF\Facade::class,
-        'Alert' => RealRashid\SweetAlert\Facades\Alert::class,
+        'Alert' => UxWeb\SweetAlert\SweetAlert::class,
 
     ],
 
